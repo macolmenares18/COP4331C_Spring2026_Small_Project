286bf14a9033e4ce29c8a6fa205709823d167df2
Maria Colmenares <macolmenares18@gmail.com>
2026-05-07 17:58:05 -0400

    Making the app easy to view and test again since there's no domain name anymore

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,2 +1,2 @@
-// const BASE_ENDPOINT = "http://localhost:8000/api";
-const BASE_ENDPOINT = "http://project.cop4331.cc/api";
+const BASE_ENDPOINT = "http://localhost:8000/api";
+// const BASE_ENDPOINT = "http://project.cop4331.cc/api";
